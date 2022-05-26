49d61f722290df378df874e77ab0daabff4505f4
indraryadi <indraaryadikadek@gmail.com>
2022-05-25 20:40:45 -0400

    test spark read from local/hdfs and save to postgre success

--- connection/submittohdfs.py
+++ connection/submittohdfs.py
@@ -1,9 +1,31 @@
 import findspark
 findspark.init('/home/hadoop/spark-3.0.3-bin-hadoop3.2/')
 from pyspark.sql import SparkSession
-spark=SparkSession.builder.appName("Submitted2").getOrCreate()
+import os
+
+spark=SparkSession.builder.appName("Submitted2").config("spark.jars", "file:///home/hadoop/postgresql-42.2.6.jar").getOrCreate()
 print(spark)
+
+#load from hdfs
 df=spark.read.format("csv").option("header",True).option("separator",",").load("hdfs:///titanic.csv")
+
+# load from local
+# df=spark.read.format("csv").option("header",True).option("separator",",").load("file:///home/hadoop/Documents/ETL_Batch_Processing-COVID19/dataset/data_covid.json")
+
+#save to hdfs
 # df.write.mode("overwrite").csv("hdfs:///covid19/raw_data_test.csv")
 # df.write.csv("hdfs:///covid19/raw_data_test2.csv")
-df.show(10,False)   
+
+#save to postgre
+mode = "overwrite"
+url = "jdbc:postgresql://localhost:5432/covid19"
+properties = {"user": "postgres","password": "indra24","driver": "org.postgresql.Driver"}
+
+df.write.jdbc(url=url, table="test_result", mode=mode, properties=properties)
+
+# df.write \
+#     .jdbc("jdbc:postgresql://localhost:5432/covid19", "public.tablename",
+#           properties={"user": "postgres", "password": "indra24"})
+
+
+# df.show(10,False)   
