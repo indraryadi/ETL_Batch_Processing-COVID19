94fd68e946e05b57ebc17dd202ef4a283f13d29f
indraryadi <indraaryadikadek@gmail.com>
2022-05-28 02:36:17 -0400

    dim case done

--- transform.py
+++ transform.py
@@ -1,14 +1,17 @@
+from struct import Struct
 import findspark
+from sqlalchemy import null
 findspark.init('/home/hadoop/spark-3.0.3-bin-hadoop3.2/')
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-
-spark=SparkSession.builder.appName("Submitted2").config("spark.jars", "file:///home/hadoop/postgresql-42.2.6.jar").getOrCreate()
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+spark=SparkSession.builder.appName('Submitted2').config('spark.jars', 'file:///home/hadoop/postgresql-42.2.6.jar').getOrCreate()
 print(spark)
 
 # load from hdfs
-rawdf=spark.read.format("csv").option("header",True).option("separator",",").load("hdfs:///covid19/raw_data_airflow2")
+rawdf=spark.read.format('csv').option('inferSchema','True').option('header',True).option('separator',',').load('hdfs:///covid19/raw_data_airflow2')
 
 #dim province
 df=rawdf.select('kode_prov','nama_prov').distinct()
@@ -24,32 +27,53 @@ df1=df1.distinct()
 # df1.show(5,truncate=False)
 
 #dim case
-df2=rawdf.select("suspect_diisolasi", "suspect_discarded", "closecontact_dikarantina", "closecontact_discarded", "probable_diisolasi", "probable_discarded", "confirmation_sembuh", "confirmation_meninggal", "suspect_meninggal", "closecontact_meninggal", "probable_meninggal")
+df2=rawdf.select(['suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+
+# a=['suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal']
+
+
+# col1=[]
+# for i in a:
+#     col1.append(i)
+#     col1.append(i.strip("'"))
+# print(col1)
+
 col=['id','status_name','status_detail','status']
 df2=df2.limit(1)
-# df2.show()
-df2=df2.toPandas()
-df2=df2.melt(var_name='status',value_name='total')
-id=[x for x in range(1,len(df2)+1)]
-# print(id)
-df2['id']=id
-df2[['status_name','status_detail']]=df2['status'].str.split('_',n=1,expand=True)
-df2=df2[col]
-# print(df2)
-df2t=spark.createDataFrame(data=df2)
+unpivot=df2.selectExpr("stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)")
+unpivot=unpivot.distinct().sort('status')
+
+w= Window.orderBy('status')
+newdf=unpivot.withColumn("id",row_number().over(w))
+newdf=newdf.select(['id','status'])
+newdf=newdf.withColumn('status_name',split(newdf['status'],'_').getItem(0)).\
+            withColumn('status_detail',split(newdf['status'],'_').getItem(1))
+newdf.show(truncate=False)
+
+# df2[['status_name','status_detail']]=df2['status'].str.split('_',n=1,expand=True)
+# df2=df2[col]
+# # print(df2)
+# df2t=spark.createDataFrame(data=df2)
+
+
+
+
+
+
+
 
 #fact prov daily
-column_start = ["tanggal", "kode_prov", "suspect_diisolasi", "suspect_discarded", "closecontact_dikarantina", "closecontact_discarded", "probable_diisolasi", "probable_discarded", "confirmation_sembuh", "confirmation_meninggal", "suspect_meninggal", "closecontact_meninggal", "probable_meninggal"]
-column_end = ['date', 'province_id', 'status', 'total']
+# column_start = ['tanggal', 'kode_prov', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal']
+# column_end = ['date', 'province_id', 'status', 'total']
 
 # AGGREGATE
-data=rawdf.toPandas()
-data = data[column_start]
-data = data.melt(id_vars=["tanggal", "kode_prov"], var_name="status", value_name="total").sort_values(["tanggal", "kode_prov", "status"])
+# data=rawdf.toPandas()
+# data = data[column_start]
+# data = data.melt(id_vars=['tanggal', 'kode_prov'], var_name='status', value_name='total').sort_values(['tanggal', 'kode_prov', 'status'])
 # data = data.groupby(by=['tanggal', 'kode_prov', 'status']).sum()
 # data = data.reset_index()
 
-print(data)
+# print(data)
 # # REFORMAT
 # data.columns = column_end
 # data['id'] = np.arange(1, data.shape[0]+1)
