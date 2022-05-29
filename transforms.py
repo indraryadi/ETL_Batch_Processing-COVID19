626c318bc17c9ca2872ca7d8656d871ce2836c40
indraryadi <indraaryadikadek@gmail.com>
2022-05-29 08:17:36 -0400

    dim and fact done

--- transforms.py
+++ transforms.py
@@ -98,6 +98,182 @@ def dim_case(rawDf):
         print("DIM CASE NOT CREATED!!!")
         return e
     
+#fact prov daily
+def fact_prov_daily(rawdf,dimcase):
+    
+    try:
+        print("TRY CREATE FACT TABLE...")
+        df = rawdf.select(['tanggal', 'kode_prov', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+        unpivot=df.selectExpr("tanggal","kode_prov","stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)").sort(['tanggal','kode_prov','status'])
+        unpivot=unpivot.groupBy(['tanggal', 'kode_prov', 'status']).sum('count')
+        # unpivot.where(unpivot["tanggal"]=="2020-08-05").show(truncate=False)
+        w= Window.orderBy('tanggal')
+        newdf=unpivot.withColumn("id",row_number().over(w))
+        # newdf2.show()
+        # print(newdf2.count())
+        #join
+        dimcase=dimcase.withColumnRenamed("id","case_id")
+        # dimcase.show()
+        newdf=newdf.join(dimcase,on="status",how="inner")
+        newdf=newdf.select(["id","kode_prov","case_id","tanggal","sum(count)"])
+        newdf=newdf.withColumnRenamed("kode_prov","province_id").\
+                      withColumnRenamed("tanggal","date").\
+                      withColumnRenamed("sum(count)","total")
+        print("FACT TABLE CREATED!!!")
+        return newdf
+    except (Exception) as e:
+        print("FACT TABLE NOT CREATED!!!")
+        return e
+    
+#fact prov monthly
+def fact_prov_monthly(rawdf,dimcase):
+    
+    try:
+        print("TRY CREATE FACT TABLE...")
+        df = rawdf.select(['tanggal', 'kode_prov', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+        unpivot=df.selectExpr("tanggal","kode_prov","stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)").sort(['tanggal','kode_prov','status'])
+        # unpivot=unpivot.withColumn('month',split(unpivot['tanggal'],'-').getItem(1))
+        unpivot=unpivot.withColumn('month',substring('tanggal',1,7))
+        unpivot=unpivot.withColumn('tanggal',unpivot['month'])
+        unpivot=unpivot.groupBy(['tanggal', 'kode_prov', 'status']).sum('count')
+        unpivot.drop("month")
+        # unpivot.where(unpivot["tanggal"]=="2020-08-05").show(truncate=False)
+        w= Window.orderBy('tanggal')
+        newdf=unpivot.withColumn("id",row_number().over(w))
+        # newdf2.show()
+        # print(newdf2.count())
+        #join
+        dimcase=dimcase.withColumnRenamed("id","case_id")
+        # dimcase.show()
+        newdf=newdf.join(dimcase,on="status",how="inner")
+        newdf=newdf.select(["id","kode_prov","case_id","tanggal","sum(count)"])
+        newdf=newdf.withColumnRenamed("kode_prov","province_id").\
+                      withColumnRenamed("tanggal","month").\
+                      withColumnRenamed("sum(count)","total")
+        print("FACT TABLE CREATED!!!")
+        return newdf
+    except (Exception) as e:
+        print("FACT TABLE NOT CREATED!!!")
+        return e
+
+#fact prov yearly
+def fact_prov_yearly(rawdf,dimcase):
+    
+    try:
+        print("TRY CREATE FACT TABLE...")
+        df = rawdf.select(['tanggal', 'kode_prov', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+        unpivot=df.selectExpr("tanggal","kode_prov","stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)").sort(['tanggal','kode_prov','status'])
+        unpivot=unpivot.withColumn('year',split(unpivot['tanggal'],'-').getItem(0))
+        unpivot=unpivot.withColumn('tanggal',unpivot['year'])
+        unpivot=unpivot.groupBy(['tanggal', 'kode_prov', 'status']).sum('count')
+        unpivot.drop("year")
+        # unpivot.where(unpivot["tanggal"]=="2020-08-05").show(truncate=False)
+        w= Window.orderBy('tanggal')
+        newdf=unpivot.withColumn("id",row_number().over(w))
+        # newdf2.show()
+        # print(newdf2.count())
+        #join
+        dimcase=dimcase.withColumnRenamed("id","case_id")
+        # dimcase.show()
+        newdf=newdf.join(dimcase,on="status",how="inner")
+        newdf=newdf.select(["id","kode_prov","case_id","tanggal","sum(count)"])
+        newdf=newdf.withColumnRenamed("kode_prov","province_id").\
+                      withColumnRenamed("tanggal","year").\
+                      withColumnRenamed("sum(count)","total")
+        print("FACT TABLE CREATED!!!")
+        return newdf
+    except (Exception) as e:
+        print("FACT TABLE NOT CREATED!!!")
+        return e
+    
+ #fact district daily
+def fact_district_daily(rawdf,dimcase):
+    
+    try:
+        print("TRY CREATE FACT TABLE...")
+        df = rawdf.select(['tanggal', 'kode_kab', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+        unpivot=df.selectExpr("tanggal","kode_kab","stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)").sort(['tanggal','kode_kab','status'])
+        unpivot=unpivot.groupBy(['tanggal', 'kode_kab', 'status']).sum('count')
+        # unpivot.where(unpivot["tanggal"]=="2020-08-05").show(truncate=False)
+        w= Window.orderBy('tanggal')
+        newdf=unpivot.withColumn("id",row_number().over(w))
+        # newdf2.show()
+        # print(newdf2.count())
+        #join
+        dimcase=dimcase.withColumnRenamed("id","case_id")
+        # dimcase.show()
+        newdf=newdf.join(dimcase,on="status",how="inner")
+        newdf=newdf.select(["id","kode_kab","case_id","tanggal","sum(count)"])
+        newdf=newdf.withColumnRenamed("kode_kab","district_id").\
+                      withColumnRenamed("tanggal","date").\
+                      withColumnRenamed("sum(count)","total")
+        print("FACT TABLE CREATED!!!")
+        return newdf
+    except (Exception) as e:
+        print("FACT TABLE NOT CREATED!!!")
+        return e   
+
+#fact district monthly
+def fact_district_monthly(rawdf,dimcase):
+    
+    try:
+        print("TRY CREATE FACT TABLE...")
+        df = rawdf.select(['tanggal', 'kode_kab', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+        unpivot=df.selectExpr("tanggal","kode_kab","stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)").sort(['tanggal','kode_kab','status'])
+        # unpivot=unpivot.withColumn('month',split(unpivot['tanggal'],'-').getItem(1))
+        unpivot=unpivot.withColumn('month',substring('tanggal',1,7))
+        unpivot=unpivot.withColumn('tanggal',unpivot['month'])
+        unpivot=unpivot.groupBy(['tanggal', 'kode_kab', 'status']).sum('count')
+        unpivot.drop("month")
+        # unpivot.where(unpivot["tanggal"]=="2020-08-05").show(truncate=False)
+        w= Window.orderBy('tanggal')
+        newdf=unpivot.withColumn("id",row_number().over(w))
+        # newdf2.show()
+        # print(newdf2.count())
+        #join
+        dimcase=dimcase.withColumnRenamed("id","case_id")
+        # dimcase.show()
+        newdf=newdf.join(dimcase,on="status",how="inner")
+        newdf=newdf.select(["id","kode_kab","case_id","tanggal","sum(count)"])
+        newdf=newdf.withColumnRenamed("kode_kab","district_id").\
+                      withColumnRenamed("tanggal","month").\
+                      withColumnRenamed("sum(count)","total")
+        print("FACT TABLE CREATED!!!")
+        return newdf
+    except (Exception) as e:
+        print("FACT TABLE NOT CREATED!!!")
+        return e
+
+#fact district yearly
+def fact_district_yearly(rawdf,dimcase):
+    
+    try:
+        print("TRY CREATE FACT TABLE...")
+        df = rawdf.select(['tanggal', 'kode_kab', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
+        unpivot=df.selectExpr("tanggal","kode_kab","stack(11,'suspect_diisolasi', suspect_diisolasi, 'suspect_discarded', suspect_discarded, 'closecontact_dikarantina', closecontact_dikarantina, 'closecontact_discarded', closecontact_discarded, 'probable_diisolasi', probable_diisolasi, 'probable_discarded', probable_discarded, 'confirmation_sembuh', confirmation_sembuh, 'confirmation_meninggal', confirmation_meninggal, 'suspect_meninggal', suspect_meninggal, 'closecontact_meninggal', closecontact_meninggal, 'probable_meninggal', probable_meninggal) as (status,count)").sort(['tanggal','kode_kab','status'])
+        unpivot=unpivot.withColumn('year',split(unpivot['tanggal'],'-').getItem(0))
+        unpivot=unpivot.withColumn('tanggal',unpivot['year'])
+        unpivot=unpivot.groupBy(['tanggal', 'kode_kab', 'status']).sum('count')
+        unpivot.drop("year")
+        # unpivot.where(unpivot["tanggal"]=="2020-08-05").show(truncate=False)
+        w= Window.orderBy('tanggal')
+        newdf=unpivot.withColumn("id",row_number().over(w))
+        # newdf2.show()
+        # print(newdf2.count())
+        #join
+        dimcase=dimcase.withColumnRenamed("id","case_id")
+        # dimcase.show()
+        newdf=newdf.join(dimcase,on="status",how="inner")
+        newdf=newdf.select(["id","kode_kab","case_id","tanggal","sum(count)"])
+        newdf=newdf.withColumnRenamed("kode_kab","district_id").\
+                      withColumnRenamed("tanggal","year").\
+                      withColumnRenamed("sum(count)","total")
+        print("FACT TABLE CREATED!!!")
+        return newdf
+    except (Exception) as e:
+        print("FACT TABLE NOT CREATED!!!")
+        return e
+
 #LOAD TO POSTGRESQL
 def load_to_dwh(df,tb_name):
     # import json
@@ -125,43 +301,56 @@ def load_to_dwh(df,tb_name):
     except (Exception) as e:
         print("LOAD TO DWH FAILED!!!")
         return e
-
-
-
-#fact prov daily
-# df = rawdf.select(['tanggal', 'kode_prov', 'suspect_diisolasi', 'suspect_discarded', 'closecontact_dikarantina', 'closecontact_discarded', 'probable_diisolasi', 'probable_discarded', 'confirmation_sembuh', 'confirmation_meninggal', 'suspect_meninggal', 'closecontact_meninggal', 'probable_meninggal'])
-
-# column_end = ['date', 'province_id', 'status', 'total']
-
-# AGGREGATE
-# data=rawdf.toPandas()
-# data = data[column_start]
-# data = data.melt(id_vars=['tanggal', 'kode_prov'], var_name='status', value_name='total').sort_values(['tanggal', 'kode_prov', 'status'])
-# data = data.groupby(by=['tanggal', 'kode_prov', 'status']).sum()
-# data = data.reset_index()
-
-# print(data)
-# # REFORMAT
-# data.columns = column_end
-# data['id'] = np.arange(1, data.shape[0]+1)
-# # MERGE
-# dim_case = dim_case.rename({'id': 'case_id'}, axis=1)
-# data = pd.merge(data, dim_case, how='inner', on='status')
-
-# data = data[['id', 'province_id', 'case_id', 'date', 'total']]
-
+    
 if __name__=="__main__":
     spark=sparkSession()
     data=loadData(spark)
     dim_prov=dim_province(data)
     dim_disc=dim_distric(data)
     dim_cases=dim_case(data)
-    load=load_to_dwh(dim_prov,'dim_province')
-    # dim_prov.show()
-    # dim_disc.show()
-    # dim_cases.show()
-    print(load)
+    fact_province_daily=fact_prov_daily(data,dim_cases)
+    fact_province_month=fact_prov_monthly(data,dim_cases)
+    fact_province_year=fact_prov_yearly(data,dim_cases)
+    fact_disc_daily=fact_district_daily(data,dim_cases)
+    fact_disc_month=fact_district_monthly(data,dim_cases)
+    fact_disc_year=fact_district_yearly(data,dim_cases)
     
+    load00=load_to_dwh(dim_disc,'dim_district')
+    print(load00)
+    
+    load01=load_to_dwh(dim_cases,'dim_case')
+    print(load01)
+    
+    
+    load02=load_to_dwh(dim_prov,'dim_province')
+    print(load02)
+    
+    # load dim case from postgre or function???
+    
+    fact_province_daily.show()
+    load1=load_to_dwh(fact_province_daily,'fact_province_daily')
+    print(load1)
+    
+    fact_province_month.show()
+    load2=load_to_dwh(fact_province_month,'fact_province_monthly')
+    print(load2)
+    
+    fact_province_year.show()
+    load3=load_to_dwh(fact_province_year,'fact_province_yearly')
+    print(load3)
+    
+    fact_disc_daily.show()
+    load11=load_to_dwh(fact_disc_daily,'fact_district_daily')
+    print(load11)
+    
+    fact_disc_month.show()
+    load22=load_to_dwh(fact_disc_month,'fact_district_monthly')
+    
+    print(load22)
+    
+    fact_disc_year.show()
+    load33=load_to_dwh(fact_disc_year,'fact_district_yearly')
+    print(load33)
     
     # ./spark-3.0.3-bin-hadoop3.2/bin/spark-submit --master yarn --queue dev --driver-class-path /home/hadoop/postgresql-42.2.6.jar --jars /home/hadoop/postgresql-42.2.6.jar ~/Documents/ETL_Batch_Processing-COVID19/transforms.py
 # 
